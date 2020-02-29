595ab126b90e69e2f68fbbc9e4eccca0a1d3718d
adamvarone <varonea1@tcnj.edu>
2020-02-29 15:34:11 -0500

    added functions

--- send_email.py
+++ send_email.py
@@ -1,20 +1,19 @@
 import smtplib, ssl
 
 port = 465  # For SSL
-receiver_email = input("Type email and press enter: ")
+receiver_email = input("Enter your email: ")
 receiver_number = input("Enter your phone number: ")
 receiver_carrier = input("Enter your phone carrier: ")
 
 if (receiver_carrier == "AT&T"):
     receiver = receiver_number + "@txt.att.net"
 elif (receiver_carrier == "verizon"):
-    receiver = receiver_number + "@vtxt.com"
+    receiver = receiver_number + "@vtext.com"
 elif (receiver_carrier == "sprint"):
     receiver = receiver_number + "@messaging.sprintpcs.com"
 elif (receiver_carrier == "t-mobile"):
     receiver = receiver_number + "@tmomail.net"
 
-sender_email = "SABREIIIHackTCNJ2020@gmail.com"
 message = """\
 Subject: Your class is open!
 
@@ -23,7 +22,15 @@ Your Welcome."""
 # Create a secure SSL context
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login("SABREIIIHackTCNJ2020@gmail.com", "Hacktcnj2020")
-    server.sendmail(sender_email, receiver_email, message)
-    server.sendmail(sender_email, receiver, message)
+def send_email(receiver, msg):
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login("SABREIIIHackTCNJ2020@gmail.com", "Hacktcnj2020")
+        server.sendmail("SABREIIIHackTCNJ2020@gmail.com", receiver, msg)
+
+def send_text(receiver, msg):
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login("SABREIIIHackTCNJ2020@gmail.com", "Hacktcnj2020")
+        server.sendmail("SABREIIIHackTCNJ2020@gmail.com", receiver, msg)
+
+#send_email(receiver_email, message)
+send_text(receiver, message)
