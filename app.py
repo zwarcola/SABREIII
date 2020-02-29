e9e80b5f71fee40edc1761d3c6944f07ebce48c9
adamvarone <varonea1@tcnj.edu>
2020-02-29 16:13:24 -0500

    combined files app and send_email

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+from send_email import send_text, send_email
 
 sg.theme('BlueMono')	# Add a touch of color
 # All the stuff inside your window.
@@ -9,10 +10,16 @@ layout = [  [sg.Text('SabreIII')],
             [sg.Text('Course Number:'), sg.InputText()],
             [sg.Text('Semester Year:'), sg.InputText()],
             [sg.Text('Semester Season:'), sg.Combo(['Fall', 'Winter', 'Spring', 'Summer'])],
-            [sg.Text('Phone Provider:'), sg.Combo(['AT&T', 'Verizon', 'Sprint', 'T-Mobil'])],
+            [sg.Text('Phone Provider:'), sg.Combo(['AT&T', 'Verizon', 'Sprint', 'T-mobile'])],
             [sg.Text('Phone Number:'), sg.InputText()],
+            [sg.Text('Receiving Email:'), sg.InputText()],
             [sg.Text('Notifications:'), sg.Combo(['Both', 'Email', 'Text'])],
             [sg.Button('Submit'), sg.Button('Cancel')] ]
+
+message = """\
+Subject: Your class is open!
+
+Your Welcome."""
 
 # Create the Window
 window = sg.Window('SabreIII GUI', layout)
@@ -21,7 +28,7 @@ while True:
     event, values = window.read()
     if event in (None, 'Cancel'):	# if user closes window or clicks cancel
         break
-    thisdict =	{
+    credentials =	{
         "username": values[0],
         "password": values[1],
         "subject": values[2],
@@ -30,10 +37,20 @@ while True:
         "semester": values[5],
         "carrier": values[6],
         "phone": values[7],
-        "notif":values[8]
+        "email": values[8],
+        "notif":values[9]
     }
-    print(thisdict)
+    print(credentials)
+    if (credentials["carrier"] == "AT&T"):
+        receiver = credentials["phone"] + "@txt.att.net"
+    elif (credentials["carrier"] == "Verizon"):
+        receiver = credentials["phone"] + "@vtext.com"
+    elif (credentials["carrier"] == "Sprint"):
+        receiver = credentials["phone"] + "@messaging.sprintpcs.com"
+    elif (credentials["carrier"] == "T-Mobile"):
+        receiver = credentials["phone"] + "@tmomail.net"
 
-
+    send_text(receiver, message)
+    send_email(credentials["email"], message)
 
 window.close()
--- send_email.py
+++ send_email.py
@@ -1,23 +1,9 @@
 import smtplib, ssl
 
 port = 465  # For SSL
-receiver_email = input("Enter your email: ")
-receiver_number = input("Enter your phone number: ")
-receiver_carrier = input("Enter your phone carrier: ")
-
-if (receiver_carrier == "AT&T"):
-    receiver = receiver_number + "@txt.att.net"
-elif (receiver_carrier == "verizon"):
-    receiver = receiver_number + "@vtext.com"
-elif (receiver_carrier == "sprint"):
-    receiver = receiver_number + "@messaging.sprintpcs.com"
-elif (receiver_carrier == "t-mobile"):
-    receiver = receiver_number + "@tmomail.net"
-
-message = """\
-Subject: Your class is open!
-
-Your Welcome."""
+# receiver_email = input("Enter your email: ")
+# receiver_number = input("Enter your phone number: ")
+# receiver_carrier = input("Enter your phone carrier: ")
 
 # Create a secure SSL context
 context = ssl.create_default_context()
@@ -31,6 +17,3 @@ def send_text(receiver, msg):
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login("SABREIIIHackTCNJ2020@gmail.com", "Hacktcnj2020")
         server.sendmail("SABREIIIHackTCNJ2020@gmail.com", receiver, msg)
-
-#send_email(receiver_email, message)
-send_text(receiver, message)
