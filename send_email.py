9d2f4b05d868035424f10726ff447cea5b13ff66
adamvarone <varonea1@tcnj.edu>
2020-02-29 14:49:48 -0500

    added phone carrier info

--- send_email.py
+++ send_email.py
@@ -1,11 +1,22 @@
 import smtplib, ssl
 
 port = 465  # For SSL
-password = input("Type your password and press enter: ")
-sender_email = "varonea1njtg@gmail.com"
-receiver_email = "varonea14@gmail.com"
+receiver_email = input("Type email and press enter: ")
+receiver_number = input("Enter your phone number: ")
+receiver_carrier = input("Enter your phone carrier: ")
+
+if (receiver_carrier == "AT&T"):
+    receiver = receiver_number + "@txt.att.net"
+elif (receiver_carrier == "verizon"):
+    receiver = receiver_number + "@vtxt.com"
+elif (receiver_carrier == "sprint"):
+    receiver = receiver_number + "@messaging.sprintpcs.com"
+elif (receiver_carrier == "t-mobile"):
+    receiver = receiver_number + "@tmomail.net"
+
+sender_email = "SABREIIIHackTCNJ2020@gmail.com"
 message = """\
-Subject: Your Class is Open!
+Subject: Your class is open!
 
 Your Welcome."""
 
@@ -13,5 +24,6 @@ Your Welcome."""
 context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login("varonea1njtg@gmail.com", password)
+    server.login("SABREIIIHackTCNJ2020@gmail.com", "Hacktcnj2020")
     server.sendmail(sender_email, receiver_email, message)
+    server.sendmail(sender_email, receiver, message)
