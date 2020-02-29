6e9af34273ece4336ff7ba054d4b121c9a2630cd
zwarcola <warcolz1@tcnj.edu>
2020-02-29 17:14:36 -0500

    Selenium functionality

--- class_checker.py
+++ class_checker.py
@@ -1,7 +1,1 @@
-from lib import getPage, login
-
-baseURL = "https://paws.tcnj.edu/psp/paws/?cmd=login"
-
-cookies = login(baseURL, "warcolz1", "Sugaree1!")
-
-print(cookies)
+from lib import login
--- lib.py
+++ lib.py
@@ -1,24 +1,84 @@
 import requests
 from bs4 import BeautifulSoup as bs
-
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.webdriver.chrome.options import Options
+
+LOGIN_REDIRECT_URL = 'https://paws.tcnj.edu/psp/paws/?cmd=login&languageCd=ENG&'
+URL = 'https://paws.tcnj.edu'
+
+credentials = {
+    'username' : 'warcolz1',
+    'password' : 'Sugaree1!'
+}
 
 def login(credentials):
+    #begin login process
     user = credentials['username']
     pwd = credentials['password']
 
-    driver = webdriver.Chrome(executable_path = '/chromedriver.exe')
+    # chrome_options = Options()
+    # chrome_options.add_argument("--disable-extensions")
+    # chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_argument("--headless")
+
+    driver = webdriver.Chrome()#options=chrome_options)
     driver.get(URL)
 
-    with webdriver.Chrome() as driver:
-        wait = WebDriverWait(driver, 10)
-        driver.get("https://google.com/ncr")
-        driver.find_element_by_name("q").send_keys("cheese" + Keys.RETURN)
-        first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3>div")))
-        print(first_result.get_attribute("textContent"))
+    while True:
+        time.sleep(2)
+        if LOGIN_REDIRECT_URL == driver.current_url:
+           print("Opening...")
+           break;
 
-login()
+    try:
+        print('Attempting Login...')
+        #get username field
+        loginForm = driver.find_element_by_xpath('//*[@id="userid"]')
+        loginForm.send_keys(user)
+
+        #get password field
+        passwordForm = driver.find_element_by_xpath('//*[@id="pwd"]')
+        passwordForm.click()
+        passwordForm.send_keys(pwd)
+
+        #get submit button
+        submit = driver.find_element_by_xpath('//*[@name="submit"]')
+        submit.click()
+        print("Logging in...")
+    except:
+        print("ERROR GETTING LOGIN FORM / LOGGING IN!!!")
+
+    html_source = driver.page_source
+
+    print('Login sucess!')
+    time.sleep(2)
+
+    return driver
+
+def searchCourse(credentials, driver):
+    #Navigate to Student Center
+    link = driver.find_element_by_link_text('Student Center')
+    link.click()
+    time.sleep(0.2)
+
+    #Open search menu
+    driver.switch_to.frame(driver.find_element_by_name('TargetContent'))
+    search = driver.find_element_by_link_text('Search') #DERIVED_SSS_SCL_SSS_GO_4$83$
+    search.click()
+    time.sleep(0.2)
+
+    #Enter class credentials
+    if(credentials['class_num']){
+    
+    }
+
+
+driver = login(credentials)
+searchCourse(credentials, driver)
+time.sleep(2)
+driver.close()
