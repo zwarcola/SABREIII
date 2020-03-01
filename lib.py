550ad432f68337e29ee90deac1a8372e90937f8b
zwarcola <warcolz1@tcnj.edu>
2020-02-29 20:11:27 -0500

    more

--- lib.py
+++ lib.py
@@ -16,8 +16,8 @@ credentials = {
     'password' : 'Sugaree1!',
     "subject": 'CSC',
     "class_num": '41817',
-    "year": '',
-    "semester": '',
+    "year": '2020',
+    "semester": 'Spring',
     "carrier": '',
     "phone_num": '',
     "email": '',
@@ -38,6 +38,7 @@ def login(credentials):
     driver = webdriver.Chrome() #options=chrome_options
     driver.get(URL)
 
+    #await redirect from chrome driver
     while True:
         time.sleep(2)
         if LOGIN_REDIRECT_URL == driver.current_url:
@@ -61,6 +62,7 @@ def login(credentials):
         print("Logging in...")
     except:
         print("ERROR GETTING LOGIN FORM / LOGGING IN!!!")
+        return -1
 
     html_source = driver.page_source
 
@@ -70,47 +72,75 @@ def login(credentials):
     return driver
 
 def searchCourse(credentials, driver):
-    #Navigate to Student Center
-    link = driver.find_element_by_link_text('Student Center')
-    link.click()
-    time.sleep(0.2)
+    try:
+        #Navigate to Student Center
+        link = driver.find_element_by_link_text('Student Center')
+        link.click()
+        time.sleep(0.2)
 
-    #Open search menu
-    driver.switch_to.frame(driver.find_element_by_name('TargetContent'))
-    search = driver.find_element_by_link_text('Search') #DERIVED_SSS_SCL_SSS_GO_4$83$
-    search.click()
-    time.sleep(3)
+        #Open search menu
+        driver.switch_to.frame(driver.find_element_by_name('TargetContent'))
+        search = driver.find_element_by_link_text('Search') #DERIVED_SSS_SCL_SSS_GO_4$83$
+        search.click()
+        time.sleep(3)
 
-    #Uncheck "Show Open Classes Only"
-    closed = driver.find_element_by_name("SSR_CLSRCH_WRK_SSR_OPEN_ONLY$3")
-    closed.click()
-    time.sleep(0.2)
+        #Uncheck "Show Open Classes Only"
+        closed = driver.find_element_by_name("SSR_CLSRCH_WRK_SSR_OPEN_ONLY$3")
+        closed.click()
+        time.sleep(0.2)
 
-    #Select the proper course subject
-    courseSelect = driver.find_element_by_xpath("//select[@id='SSR_CLSRCH_WRK_SUBJECT_SRCH$0']")
-    courseSelect.click()
-    time.sleep(0.2)
-    for option in courseSelect.find_elements_by_tag_name('option'):
-        if option.get_attribute('value') == credentials['subject']:
-            time.sleep(0.1)
-            option.click()
-            break
-    time.sleep(0.2)
+        #select term and year
+        termSelect = driver.find_element_by_xpath('//*[@id="CLASS_SRCH_WRK2_STRM$35$"]')
+        termSelect.click()
+        time.sleep(0.2)
+        Found = False
+        for option in termSelect.find_elements_by_tag_name('option'):
+            if option.get_attribute('innerHTML') == (credentials['year'] + ' ' + credentials['semester']):
+                time.sleep(0.1)
+                Found = True
+                option.click()
+                break
 
-    #Enter course number
-    courseNum = driver.find_element_by_name('SSR_CLSRCH_WRK_CLASS_NBR$7')
-    courseNum.send_keys(credentials['class_num'])
-    courseNum.send_keys(Keys.RETURN)
-    time.sleep(2)
+        if not Found:
+            print('ERROR FINDING YEAR AND SEMESTER')
 
-    #open the class info
-    classLink = driver.find_element_by_link_text(credentials['class_num'])
-    classLink.click()
-    time.sleep(2)
+        time.sleep(0.2)
 
-    #get amount of availible seats
-    availibleSeats = driver.find_element_by_xpath('//*[@id="win0divSSR_CLS_DTL_WRK_AVAILABLE_SEATS"]')
-    availibleSeats = availibleSeats.find_element_by_class_name('PSEDITBOX_DISPONLY').get_attribute('innerHTML')
+        #Select the proper course subject
+        courseSelect = driver.find_element_by_xpath("//select[@id='SSR_CLSRCH_WRK_SUBJECT_SRCH$0']")
+        courseSelect.click()
+        time.sleep(0.2)
+        Found = False
+        for option in courseSelect.find_elements_by_tag_name('option'):
+            if option.get_attribute('value') == credentials['subject']:
+                time.sleep(0.1)
+                Found = True
+                option.click()
+                break
+
+        if not Found:
+            print('ERROR FINDING CLASS SUBJECT')
+
+        time.sleep(0.2)
+
+        #Enter course number
+        courseNum = driver.find_element_by_name('SSR_CLSRCH_WRK_CLASS_NBR$7')
+        courseNum.send_keys(credentials['class_num'])
+        courseNum.send_keys(Keys.RETURN)
+        time.sleep(2)
+
+        #open the class info
+        classLink = driver.find_element_by_link_text(credentials['class_num'])
+        classLink.click()
+        time.sleep(2)
+
+        #get amount of availible seats
+        availibleSeats = driver.find_element_by_xpath('//*[@id="win0divSSR_CLS_DTL_WRK_AVAILABLE_SEATS"]')
+        availibleSeats = availibleSeats.find_element_by_class_name('PSEDITBOX_DISPONLY').get_attribute('innerHTML')
+
+    except:
+        print("ERROR WHILE FINDING COURSE")
+        return -1
 
     #return total number
     return availibleSeats
